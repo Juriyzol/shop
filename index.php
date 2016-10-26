3a09cbd7b570a9e0623b0e48911a34bfa2a48800
Juriy <work.fse@gmail.com>
2016-10-26 23:01:10 +0300

    ,,,,

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// main
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// include files
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Router.php');
+
+// db
+
+// router
+
+$router = new Router();
+$router->run();
+
+
+
+
+
+
+
+
+
