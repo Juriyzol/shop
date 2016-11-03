8e3802d5b4451c136e97a630904f2fd202a89b9a
Juriy <work.fse@gmail.com>
2016-11-03 11:31:08 +0200

    исправлена ошибка роутов, получены параметры для методов

--- components/Router.php
+++ components/Router.php
@@ -22,11 +22,17 @@ class Router
 	{
 		$uri = $this->getURI();
 		
-		foreach ($this->routes as $uriPattern => $path)
-		{			
-			if (preg_match("~$uriPattern~", $uri))
+		foreach ($this->routes as $uriPattern => $path) // '/news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2'
+		{	
+			if (preg_match("~$uriPattern~", $uri)) // для совпадения с шаблоном достаточно любого набора букв/цифр
 			{ 
 				$internalRoute = preg_replace("~$uriPattern~", $path, $uri);
+				/* 
+				При помощи функции preg_replace() мы по определенному шаблону
+				(означающему поиск любых букв и цифр) описанному в переменной 
+				"~$uriPattern~" ищем совпадения в строке запроса $uri и подставляем
+				эти значения на места подмасок в переменной $path
+				*/
 				
 				echo 'Переменная $internalRoute до explode(): <br>';
 				echo '<pre>';
@@ -40,6 +46,7 @@ class Router
 				print_r($segments);
 				echo '</pre> <br>';
 				
+				//$bagName = array_shift($segments); 
 				$controllerName = ucfirst(array_shift($segments)).'Controller'; 
 				$actionName = 'action'.ucfirst((array_shift($segments)));
 				
--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,7 @@
 return array (
 	
 	// news/sport/114
-	'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',	
+	'/news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',	
 	
 	/*
 	'news/([0-9]+)' => 'news/view',	
